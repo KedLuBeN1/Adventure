06529c50e5e6d69d4e16799daa7cc86e14266527
PC <PC@DESKTOP-LJTORU7>
2021-01-23 19:54:09 +0100

    c2

--- Adventure/src/main/java/lab/Main.java
+++ Adventure/src/main/java/lab/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
 		System.out.println("Main:constructor()");
 	}
 
-	//nejaka zmena
+	//nejaka zmena 2
 	public static void main(String[] args) {
 		launch(args);
 	}
